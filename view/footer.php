f8e8c1e8f28727e6aa8303953455318ab3f40655
Hidran <hidran.arias@spryker.com>
2023-03-21 23:51:22 +0100

    123. Splits up top, header and footer

---
+++ view/footer.php
@@ -0,0 +1,15 @@
+<footer class='footer mt-auto py-3 bg-light'>
+    <div class='container'>
+        <span class='text-muted'>
+            @copyright <?= date('d/m/Y') ?> </span>
+    </div>
+</footer>
+
+
+<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.2/dist/js/bootstrap.bundle.min.js'></script>
+
+
+
+</body>
+
+</html>
---
+++ view/top.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang='it' class='h-100'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='description' content='USER MANAGEMENT SYSTEM'>
+    <meta name='author' content='HIDRAN ARIAS'>
+    <title>USER MANAGEMENT SYSTEM</title>
+    <link rel='icon' href='favicon.ico' type='image/png'>
+
+    <link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet'>
+
+
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css' integrity='sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+
+
+    <link href='css/style.css' rel='stylesheet'>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2/dist/css/bootstrap.min.css' rel='stylesheet'>
+
+
+    <style>
+        main>.container {
+            padding: 60px 15px 0;
+        }
+    </style>
+
+</head>
+
+<body class='d-flex flex-column h-100'>
