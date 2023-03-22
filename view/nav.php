33faf15c09ae29bca5514af947bb6b3f7acd3677
Hidran <hidran.arias@spryker.com>
2023-03-22 16:18:58 +0100

    124. Adds link to navbar

--- view/nav.php
+++ view/nav.php
@@ -1,9 +1,16 @@
+<?php
+$currentUrl = $_SERVER['PHP_SELF'];
+$indexPage = 'index.php';
+$action = $_GET['action'] ?? '';
+$indexActive = !$action ? 'active' : '';
+$newActive = $action === 'insert' ? 'active' : '';
+?>
 <header>
     <!-- Fixed navbar -->
     <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
         <div class='container-fluid'>
             <a class='navbar-brand' href='#'>
-                <i class="fa-solid fa-users fa-lg"></i>
+                <i class="fa-solid fa-user fa-lg"></i>
             </a>
             <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'>
                 <span class='navbar-toggler-icon'></span>
@@ -11,10 +18,12 @@
             <div class='collapse navbar-collapse' id='navbarCollapse'>
                 <ul class='navbar-nav me-auto mb-2 mb-md-0'>
                     <li class='nav-item'>
-                        <a class='nav-link active' aria-current='page' href='#'>Home</a>
+                        <a class='nav-link <?=$indexActive?>' aria-current='page' href='<?=$indexPage?>'><i
+                                    class='fa-solid fa-users'></i>Users</a>
                     </li>
                     <li class='nav-item'>
-                        <a class='nav-link' href='#'>Link</a>
+                        <a class='nav-link  <?=$newActive?>' href='<?=$indexPage?>?action=insert'><i
+                                    class='fa-solid fa-user-plus'></i>New User</a>
                     </li>
                     <li class='nav-item'>
                         <a class='nav-link disabled'>Disabled</a>
