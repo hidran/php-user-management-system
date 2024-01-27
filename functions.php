7c87b951306617e59b3274a9afb10a42cb52a6d5
Hidran Arias <hidran@gmail.com>
2024-01-27 13:11:02 +0100

    . SELECT * FROM. Leggere i records utenti dal database con mysqli

--- functions.php
+++ functions.php
@@ -1,52 +1,60 @@
 <?php
 require_once 'connection.php';
+function getConfig($param)
+{
 
-function getRandName(): string {
+    $config = require 'config.php';
+
+    return array_key_exists($param, $config) ? $config[$param] : null;
+}
+function getRandName(): string
+{
     $names = [
-        'ROBERTO','GIOVANNI','GIULIA','MARIO','ALE'
+        'ROBERTO', 'GIOVANNI', 'GIULIA', 'MARIO', 'ALE'
     ];
     $lastnames = [
-        'ROSSI','RE','ARIAS','SMITH','MENDOZA','CRUZ','WILDE'
+        'ROSSI', 'RE', 'ARIAS', 'SMITH', 'MENDOZA', 'CRUZ', 'WILDE'
 
     ];
 
-    $rand1 =  random_int(0, count($names) -1) ;
-    $rand2 =  random_int(0, count($lastnames) -1);
+    $rand1 =  random_int(0, count($names) - 1);
+    $rand2 =  random_int(0, count($lastnames) - 1);
 
-    return  $names[$rand1].' '. $lastnames[$rand2] ;
+    return  $names[$rand1] . ' ' . $lastnames[$rand2];
 }
 
 //echo getRandName();
-function getRandEmail(string $name):string {
+function getRandEmail(string $name): string
+{
 
-    $domains = ['google.com','yahoo.com','hotmail.it', 'libero.it'];
+    $domains = ['google.com', 'yahoo.com', 'hotmail.it', 'libero.it'];
 
-    $rand1 =  random_int(0, count($domains) -1) ;
+    $rand1 =  random_int(0, count($domains) - 1);
 
-    return  strtolower(str_replace(' ','.',$name ).random_int(10,99).'@'.$domains[$rand1]);
-
+    return  strtolower(str_replace(' ', '.', $name) . random_int(10, 99) . '@' . $domains[$rand1]);
 }
-function getRandFiscalCode():string {
+function getRandFiscalCode(): string
+{
 
     $i = 16;
     $res = '';  // ABQZ
 
-    while ( $i > 0){
+    while ($i > 0) {
 
-        $res .= chr(random_int(65,90));
+        $res .= chr(random_int(65, 90));
 
         $i--;
-
     }
     return $res;
-
 }
-function getRandomAge(): int{
+function getRandomAge(): int
+{
     return random_int(0, 120);
 }
-function insertRandUser($totale, mysqli $conn){
+function insertRandUser($totale, mysqli $conn)
+{
 
-    while ($totale> 0) {
+    while ($totale > 0) {
 
         $username = getRandName();
         $email = getRandEmail($username);
@@ -55,10 +63,10 @@ function insertRandUser($totale, mysqli $conn){
 
         $sql = 'INSERT INTO users (username, email, fiscalcode, age) VALUES ';
         $sql .= " ('$username','$email', '$fiscalcode', $age) ";
-        echo $totale .' '.$sql.'<br>';
+        echo $totale . ' ' . $sql . '<br>';
         $res = $conn->query($sql);
         if (!$res) {
-            echo $conn->error.'<br>';
+            echo $conn->error . '<br>';
         } else {
             $totale--;
         }
@@ -69,3 +77,33 @@ function insertRandUser($totale, mysqli $conn){
  * @var \Mysqli $mysqli
  */
 //insertRandUser(30, $mysqli);
+function getUsers(array $params = [])
+{
+
+    /**
+     * @var $conn mysqli
+     */
+
+    $conn = $GLOBALS['mysqli'];
+
+    $records = [];
+
+    $limit = getConfig('recordsPerPage');
+
+    if (!$limit) {
+        $limit = 10;
+    }
+    $sql = 'SELECT * FROM users LIMIT ' . $limit;
+    // echo $sql;
+    $res = $conn->query($sql);
+    if ($res) {
+
+        while ($row = $res->fetch_assoc()) {
+            $records[] = $row;
+        }
+    }
+
+    return $records;
+}
+
+var_dump(getUsers());
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// for dev purposes
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once 'view/top.php';
 require_once 'view/nav.php';
 ?>
